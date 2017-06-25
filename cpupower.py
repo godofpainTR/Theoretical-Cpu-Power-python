3b5e5d56c32f025b77ee70f2f4e0e339d83ff218
godofpain <metekaanozden@gmail.com>
2017-06-26 00:52:38 +0300

    Added Intel Atom and AMD Ryzen

--- cpupower.py
+++ cpupower.py
@@ -1,11 +1,25 @@
 import os
 import time
+
+def MIPSF():
+    print("Enter your CPU clock (in MHz):")
+    clock = int(input())
+    print("Enter physical core count:")
+    core = int(input())
+    MIPSS = core * clock * ipcs
+    MIPSD = core * clock * ipcd
+    print(MIPSS, " MIPS (Single Precision)")
+    print(MIPSD, " MIPS (Double Precision)")
+    time.sleep(3)
+    print("Type 1 to start over or type 2 to exit")
 while True:
     print("Choose CPU brand:")
     print("1.AMD")
     print("2.Intel")
-    ipc = 1
-    ipc == int(ipc)
+    ipcs = 1
+    ipcd = 1
+    ipcs == int(ipcs)
+    ipcd == int(ipcd)
     choice = input()
     if choice == "1":
         print("Choose architecture:")
@@ -13,17 +27,12 @@ while True:
         print("2.Piledriver (FX-x3xx and 9590)")
         print("3.Bulldozer (Other FX processors)")
         print("4.Steamroller (AX series APUs)")
+        print("5.Zen (Ryzen processors)")
         choice2 = input()
         if choice2 == "1":
-            ipc = 8
-            print("Enter your CPU clock (in MHz):")
-            clock = int(input())
-            print("Enter physical core count:")
-            core = int(input())
-            MIPS = core*clock*ipc
-            print(MIPS, " MIPS")
-            time.sleep(3)
-            print("Type 1 to start over or type 2 to exit")
+            ipcs = 8
+            ipcd = int(ipcs/2)
+            MIPSF()
             choice3 = input()
             if choice3 == "1":
                 os.system("cls" if os.name == "nt" else "clear")
@@ -34,15 +43,22 @@ while True:
             else:
                 raise TypeError
         elif choice2 == "2" or "3" or "4":
-            ipc = 16
-            print("Enter your CPU clock (in MHz):")
-            clock = int(input())
-            print("Enter physical core count:")
-            core = int(input())
-            MIPS = core * clock * ipc
-            print(MIPS, " MIPS")
-            time.sleep(3)
-            print("Type 1 to start over or type 2 to exit")
+            ipcs = 8
+            ipcd = int(ipcs / 2)
+            MIPSF()
+            choice3 = input()
+            if choice3 == "1":
+                os.system("cls" if os.name == "nt" else "clear")
+                continue
+            elif choice3 == "2":
+                time.sleep(1)
+                break
+            else:
+                raise TypeError
+        elif choice2 == "5":
+            ipcs = 16
+            ipcd = int(ipcs / 2)
+            MIPSF()
             choice3 = input()
             if choice3 == "1":
                 os.system("cls" if os.name == "nt" else "clear")
@@ -65,15 +81,9 @@ while True:
         print("7.Skylake or Kaby Lake (Core i3/5/7-6xxx or 7xxx series)")
         choice5 = input()
         if choice5 == "1":
-            ipc = 8
-            print("Enter your CPU clock (in MHz):")
-            clock = int(input())
-            print("Enter physical core count:")
-            core = int(input())
-            MIPS = core * clock * ipc
-            print(MIPS, " MIPS")
-            time.sleep(3)
-            print("Type 1 to start over or type 2 to exit")
+            ipcs = 8
+            ipcd = int(ipcs / 2)
+            MIPSF()
             choice3 = input()
             if choice3 == "1":
                 os.system("cls" if os.name == "nt" else "clear")
@@ -84,15 +94,9 @@ while True:
             else:
                 raise TypeError
         elif choice5 == "2":
-            ipc = 8
-            print("Enter your CPU clock (in MHz):")
-            clock = int(input())
-            print("Enter physical core count:")
-            core = int(input())
-            MIPS = core * clock * ipc
-            print(MIPS, " MIPS")
-            time.sleep(3)
-            print("Type 1 to start over or type 2 to exit")
+            ipcs = 8
+            ipcd = int(ipcs / 2)
+            MIPSF()
             choice3 = input()
             if choice3 == "1":
                 os.system("cls" if os.name == "nt" else "clear")
@@ -103,15 +107,9 @@ while True:
             else:
                 raise TypeError
         elif choice5 == "3":
-            ipc = 16
-            print("Enter your CPU clock (in MHz):")
-            clock = int(input())
-            print("Enter physical core count:")
-            core = int(input())
-            MIPS = core * clock * ipc
-            print(MIPS, " MIPS")
-            time.sleep(3)
-            print("Type 1 to start over or type 2 to exit")
+            ipcs = 16
+            ipcd = int(ipcs / 2)
+            MIPSF()
             choice3 = input()
             if choice3 == "1":
                 os.system("cls" if os.name == "nt" else "clear")
@@ -122,15 +120,9 @@ while True:
             else:
                 raise TypeError
         elif choice5 == "4":
-            ipc = 16
-            print("Enter your CPU clock (in MHz):")
-            clock = int(input())
-            print("Enter physical core count:")
-            core = int(input())
-            MIPS = core * clock * ipc
-            print(MIPS, " MIPS")
-            time.sleep(3)
-            print("Type 1 to start over or type 2 to exit")
+            ipcs = 16
+            ipcd = int(ipcs / 2)
+            MIPSF()
             choice3 = input()
             if choice3 == "1":
                 os.system("cls" if os.name == "nt" else "clear")
@@ -141,15 +133,22 @@ while True:
             else:
                 raise TypeError
         elif choice5 == "5" or "6" or "7":
-            ipc = 32
-            print("Enter your CPU clock (in MHz):")
-            clock = int(input())
-            print("Enter physical core count:")
-            core = int(input())
-            MIPS = core * clock * ipc
-            print(MIPS, " MIPS")
-            time.sleep(3)
-            print("Type 1 to start over or type 2 to exit")
+            ipcs = 32
+            ipcd = int(ipcs / 2)
+            MIPSF()
+            choice3 = input()
+            if choice3 == "1":
+                os.system("cls" if os.name == "nt" else "clear")
+                continue
+            elif choice3 == "2":
+                time.sleep(1)
+                break
+            else:
+                raise TypeError
+        elif choice5 == "8":
+            ipcs = 4
+            ipcd = int(ipcs / 2)
+            MIPSF()
             choice3 = input()
             if choice3 == "1":
                 os.system("cls" if os.name == "nt" else "clear")
